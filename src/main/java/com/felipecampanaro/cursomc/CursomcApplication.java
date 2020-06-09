0b0c077a2c7a94f48e7a4ecdb3c50e40bdaeccb5
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-06-09 19:12:29 -0300

    Pedido, EstadoPagamento and Pagamento

--- src/main/java/com/felipecampanaro/cursomc/CursomcApplication.java
+++ src/main/java/com/felipecampanaro/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.felipecampanaro.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.felipecampanaro.cursomc.domain.Cidade;
 import com.felipecampanaro.cursomc.domain.Cliente;
 import com.felipecampanaro.cursomc.domain.Endereco;
 import com.felipecampanaro.cursomc.domain.Estado;
+import com.felipecampanaro.cursomc.domain.Pagamento;
+import com.felipecampanaro.cursomc.domain.PagamentoComBoleto;
+import com.felipecampanaro.cursomc.domain.PagamentoComCartao;
+import com.felipecampanaro.cursomc.domain.Pedido;
 import com.felipecampanaro.cursomc.domain.Produto;
+import com.felipecampanaro.cursomc.domain.enums.EstadoPagamento;
 import com.felipecampanaro.cursomc.domain.enums.TipoCliente;
 import com.felipecampanaro.cursomc.repositories.CategoriaRepository;
 import com.felipecampanaro.cursomc.repositories.CidadeRepository;
 import com.felipecampanaro.cursomc.repositories.ClienteRepository;
 import com.felipecampanaro.cursomc.repositories.EnderecoRepository;
 import com.felipecampanaro.cursomc.repositories.EstadoRepository;
+import com.felipecampanaro.cursomc.repositories.PagamentoRepository;
+import com.felipecampanaro.cursomc.repositories.PedidoRepository;
 import com.felipecampanaro.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -84,6 +96,22 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/05/2020 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/05/2020 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/05/2020 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 
---
+++ src/main/java/com/felipecampanaro/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.felipecampanaro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipecampanaro.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
