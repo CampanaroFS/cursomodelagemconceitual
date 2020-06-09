e91e11012245024a543f00b07dcaf5605b4f18ad
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-06-09 12:29:37 -0300

    Products and association manytomany

---
+++ src/main/java/com/felipecampanaro/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.felipecampanaro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.felipecampanaro.cursomc.domain.Produto;
+import com.sun.xml.bind.v2.model.core.ID;
+
+public interface ProdutoRepository extends JpaRepository<Produto, ID>{
+
+}
