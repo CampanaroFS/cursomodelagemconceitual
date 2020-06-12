de21ca2b80cc70aaf5eb1b5edfadd002e94a7766
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-06-12 19:01:56 -0300

    Deleting a Categoria with delete and Adding DataIntegrity exception

--- src/main/java/com/felipecampanaro/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipecampanaro/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.felipecampanaro.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.felipecampanaro.cursomc.domain.Categoria;
 import com.felipecampanaro.cursomc.repositories.CategoriaRepository;
+import com.felipecampanaro.cursomc.services.exceptions.DataIntegrityException;
 import com.felipecampanaro.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void deleteById(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto");
+		}
 	}
 
 }
