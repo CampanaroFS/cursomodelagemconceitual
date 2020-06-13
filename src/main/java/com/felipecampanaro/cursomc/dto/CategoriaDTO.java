41582ed0b466dee0f484f00db48f8a5aa332dd09
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-06-13 13:49:54 -0300

    Syntactic validation with Bean Validation

--- src/main/java/com/felipecampanaro/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/felipecampanaro/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.felipecampanaro.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.felipecampanaro.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -9,6 +13,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchmento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 à 80 caractéres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/felipecampanaro/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipecampanaro/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.felipecampanaro.cursomc.domain.Categoria;
+import com.felipecampanaro.cursomc.dto.CategoriaDTO;
 import com.felipecampanaro.cursomc.repositories.CategoriaRepository;
 import com.felipecampanaro.cursomc.services.exceptions.DataIntegrityException;
 import com.felipecampanaro.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
