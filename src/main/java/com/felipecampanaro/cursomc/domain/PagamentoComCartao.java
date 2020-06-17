4e8e07effaa9513a27f4ab86a61ffa1706d386b7
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-06-16 23:14:11 -0300

    Inserting Pedido

--- src/main/java/com/felipecampanaro/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/felipecampanaro/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.felipecampanaro.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.felipecampanaro.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	
 	private static final long serialVersionUID = 1L;
