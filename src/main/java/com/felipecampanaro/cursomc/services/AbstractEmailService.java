42990d4648af377d18ca35e0e34711426afe36aa
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-06-27 16:45:11 -0300

    Password forgotten

--- src/main/java/com/felipecampanaro/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/felipecampanaro/cursomc/services/AbstractEmailService.java
@@ -2,17 +2,13 @@ package com.felipecampanaro.cursomc.services;
 
 import java.util.Date;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.felipecampanaro.cursomc.domain.Cliente;
 import com.felipecampanaro.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -69,4 +65,20 @@ public abstract class AbstractEmailService implements EmailService {
 		
 		return mimeMessage;
 	}*/
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+	
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
 }
